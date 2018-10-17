373bde0500637a4fffadeac224ecafd8b0fc2f8e
Mitchell Myers <myersmk@umich.edu>
2018-10-17 21:36:39 +0000

    Game now restarts when the seagull misses a ring.

---
+++ Assets/restart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour {
+
+	GameObject bg;
+	Scene s;
+	const double minvelocity = -1.0;
+
+	// Use this for initialization
+	void Start () {
+		bg = GameObject.Find("background");
+		s = SceneManager.GetActiveScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bg.GetComponent<Rigidbody2D>().velocity.x >= minvelocity)
+		{
+			SceneManager.LoadScene(s.buildIndex);
+		}
+	}
+}
