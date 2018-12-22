e3e9374abb7997e087cb37de84a5d134696d5e7e
Mitchell Myers <myersmk@umich.edu>
2018-12-22 03:03:05 +0000

    Experimenting with Unity's auto layout system to get certain things adjusted. Works very similarly to how HTML does things, but it's a bit finicky when nesting multiple things.

---
+++ Assets/GUISpacing.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * AUTHOR: MITCHELL MYERS
+ * DATE: 12/21/2018
+ * 
+ */
+
+namespace GullGust
+{
+	public class GUISpacing : MonoBehaviour
+	{
+
+		public enum Style
+		{
+			Vertical,
+			Horizontal,
+			Diagonal
+		}
+
+		private RectTransform[] buttons;
+		private Vector2 origin;
+
+		public Style arrangement = Style.Vertical;
+		public float spacing = 15f;
+
+		private void Start()
+		{
+			origin = transform.position;
+		}
+
+		private void OnValidate()
+		{
+			buttons = GetComponentsInChildren<RectTransform>();
+			switch (arrangement)
+			{
+				case Style.Vertical:
+					ArrangeElementsVertically(ref buttons);
+					break;
+
+				case Style.Horizontal:
+					ArrangeElementsHorizontally(ref buttons);
+					break;
+
+				case Style.Diagonal:
+					ArrangeElementsDiagonally(ref buttons);
+					break;
+
+				default:
+					break;
+			}
+		}
+
+		private void ArrangeElementsVertically(ref RectTransform[] elements)
+		{
+			float yPos = origin.y + (spacing * (elements.Length / 2f));
+			RectTransform vRect;
+
+			for (int i = 0; i < elements.Length; i++)
+			{
+				yPos -= spacing * i;
+				vRect = elements[i];
+			}
+
+		}
+
+		private void ArrangeElementsHorizontally(ref RectTransform[] elements)
+		{
+
+		}
+
+		private void ArrangeElementsDiagonally(ref RectTransform[] elements)
+		{
+
+		}
+	}
+}
--- Assets/Gull Gust/Scripts/MenuManager.cs
+++ Assets/Gull Gust/Scripts/MenuManager.cs
@@ -3,59 +3,184 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+ * AUTHOR: Mitchell Myers
+ * DATE: 12/21/2018
+ * 
+ */
+
 namespace GullGust
 {
 	public class MenuManager : MonoBehaviour
 	{
-
 		private GameObject currentMenu;
-		private GameObject[] menues;
-		private Stack<GameObject> history; // for backtracking
-		public GameObject[] enablerList = new GameObject[1];
+		private Stack<GameObject> history; // for backtracking		
+		private const string DEFAULT_START_BUTTON = "Jump";
+		private const string DEFAULT_TARGET_TAG = "Menu";
+		private const string DEFAULT_RETURN_BUTTON = "Cancel";
+
+		[Header("Buttons")]
+		public GameObject GUIReturnButton; // the button to use for return to previous menues
+		public string inputReturnButton = DEFAULT_RETURN_BUTTON;
+		public string inputStartButton = DEFAULT_START_BUTTON;
+	
+		[Header("Startup")]
+		public string targetMenuTag = DEFAULT_TARGET_TAG;
+		public GameObject startMenu;
+		public GameObject mainMenu;
+
+		[Header("Debug")]
+		public bool showPanels = true;
+		
 
 		// Use this for initialization
 		void Start()
 		{
-			var x = GetComponentsInChildren<Transform>();
-			menues = GetGameObjects(x);
-			foreach (GameObject menu in menues)
-			{
-				menu.SetActive(false);
-			}
+			GameObject[] menues = GameObject.FindGameObjectsWithTag(targetMenuTag);
+			Image[] panels = GetComponentsInChildren<Image>();
 
+			DisableAll(menues);
+			DisableAll(panels);
+			Enable(startMenu);
+			currentMenu = startMenu;
 		}
 
-		private GameObject[] GetGameObjects(Component[] x)
+		private void Update()
 		{
-			var size = x.Length;
-			GameObject[] results = new GameObject[size];
-
-			for (int i = 0; i < size; i++)
-			{
-				results[i] = x[i].gameObject;
-			}
-
-			return results;
+			DetermineInputNavigation(); // let the script decide how you are allowed to move forward through menues
+			DetermineInputBacktracking(); // let the script decide how you are allowed to move backward through menues
 		}
+
+		private void OnValidate()
+		{
+			Image[] panels = GetComponentsInChildren<Image>();
+
+			if (showPanels)
+			{
+				EnableAll(panels);
+			}
+			else
+			{
+				DisableAll(panels);
+			}
+		}
+
+		private void DetermineInputNavigation()
+		{
+			if (OnStartMenu() && PressedAnyButton())
+			{
+				NavigateTo(mainMenu);
+			}
+		}
+
+		private void DetermineInputBacktracking()
+		{
+			if (!OnStartMenu() && !OnMainMenu() && PressedReturnButton())
+			{
+				Backtrack();
+			}
+		}		
+
+		private bool OnStartMenu() { return (currentMenu == startMenu); }
+		private bool OnMainMenu() { return (currentMenu == mainMenu); }
+		private bool PressedStartButton() { return Input.GetButtonDown(inputStartButton); }
+		private bool PressedReturnButton() { return Input.GetButtonDown(inputReturnButton); }
+		private bool PressedAnyButton() { return Input.anyKeyDown; }
 
 		public void Backtrack()
 		{
-			currentMenu.SetActive(false);
+			GameObject previousMenu;
+			Disable(currentMenu);
 
+			Debug.Assert(history.Count > 1);
 			history.Pop();
-			currentMenu = history.Peek();
+			previousMenu = history.Peek();
 
-			currentMenu.SetActive(true);
+			Enable(previousMenu);
+			currentMenu = previousMenu;
+
+			DetermineReturnButtonDisplay();
 		}
 
-		public void Navigate(GameObject menu)
+		private void DetermineReturnButtonDisplay()
 		{
-			currentMenu.SetActive(false);
+			if (!OnMainMenu() && !OnStartMenu())
+			{
+				Enable(GUIReturnButton);
+			}
+			else
+			{
+				Disable(GUIReturnButton);
+			}
+		}
 
-			history.Push(menu);
-			currentMenu = menu;
+		public void NavigateTo(GameObject nextMenu)
+		{
+			Disable(currentMenu);
+			history.Push(nextMenu);
 
-			currentMenu.SetActive(true);
+			Enable(nextMenu);
+			currentMenu = nextMenu;
+
+			DetermineReturnButtonDisplay();
+		}
+
+		private void Enable(GameObject x)
+		{
+			x.SetActive(true);
+		}
+
+		private void Disable(GameObject x)
+		{
+			x.SetActive(false);
+		}
+
+		private void DisableAll(GameObject[] x)
+		{
+			foreach (GameObject g in x)
+			{
+				Disable(g);
+			}
+		}
+
+		private void DisableAll(Behaviour[] x)
+		{
+			foreach (Behaviour b in x)
+			{
+				Disable(b);
+			}
+		}
+
+		private void Disable(Behaviour b)
+		{
+			b.enabled = false;
+		}
+
+		private void DisableAll(Image[] x)
+		{
+			foreach (Image i in x)
+			{
+				if (i.gameObject.CompareTag("Panel"))
+				{
+					Disable(i);
+				}
+			}
+		}
+
+		private void Enable(Behaviour b)
+		{
+			b.enabled = true;
+		}
+
+		private void EnableAll(Image[] x)
+		{
+			foreach (Image i in x)
+			{
+				if (i.gameObject.CompareTag("Panel"))
+				{
+					Enable(i);
+				}
+			}
 		}
 	}
 
